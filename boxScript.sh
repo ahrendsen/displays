970d8e86724f6071e5274ae2efa107d2ccb2ef7c
Karl Ahrendsen <karl.ahrendsen@gmail.com>
2019-09-27 16:52:09 -0500

    Check for connection, restart network if there is none.

--- boxScript.sh
+++ boxScript.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
+# This first command basically is just trying to access
+# Box to see if the internet connection is active.
+# --low-level-retries 3 makes the command return
+#   rather quickly instead of taking several seconds (~20) to 
+#   respond.
+rclone sync box:JHDisplay/ /home/unlphysics/display/ \
+	--dry-run --low-level-retries 3 2>&1
+if [ $? -gt 0 ] # The previously run command returned an error
+	then	
+	# Likely we're not connected to the internet, 
+	# we need to reconnect
+	service network-manager restart
+fi
+# Now we should be connected to the internet, record
+# the number of files that have been changed. 
 NUMCHANGEDFILES=$(rclone sync box:JHDisplay/ /home/unlphysics/display/ \
---dry-run 2>&1 | wc --lines)
+--dry-run --contimeout 15s 2>&1 | wc --lines)
+
 # If any files were changed...
 if [ $NUMCHANGEDFILES -gt 0 ]
 	then
 	# Download the file contents to the display,
 	# changing the files that are currently on the 
 	# device so that they match what is stored in Box.
-	echo "Files have been changed! Downloading..."
-        rclone sync -P box:JHDisplay/ /home/unlphysics/display/
-	echo "Files downloaded"
+	# --progress shows file transfer statistics so if 
+	#   the script is being run interactively, the
+	#   user can know that the script is working.
+	# --transfers 1 limits the number of files that
+	#   are simultaneously downloaded. One display 
+	#   had trouble updating when this was set to 
+	#   the default of 4, but seemed to have no 
+	#   trouble when it was set to 1
+	rclone sync box:JHDisplay/ /home/unlphysics/display/ --progress --transfers 1
 
 	# Save an empty file that indicates that new slides 
 	# have been downloaded and unlphysics should 
 	# run its reload script.
-	echo "Setting flag for file reload..."
 	touch /home/unlphysics/reload
-	echo "Set flag for file reload!"
 fi
